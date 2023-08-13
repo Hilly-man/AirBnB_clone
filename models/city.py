c3296d123617d67a0198de680225565ba49ddf23
Hilly-man <hilkiahe@gmail.com>
2023-08-13 11:14:07 -0700

    created class for state and city

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """class city"""
+
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This class inherits from BaseModel."""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """class representing state."""
+
+    name = ""
+
